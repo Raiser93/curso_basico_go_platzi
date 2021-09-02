c58b37292d3a6c4d26255bdb17bc96eb2ab080ce
Brayan Giraldo <bgiraldo@tecmovin.com>
2021-09-01 19:48:45 -0500

    Arrays y Slices

--- src/main.go
+++ src/main.go
@@ -267,21 +267,46 @@ func main() {
 	// 	fmt.Println("No condicion")
 	// }
 
-	// Defer
-	defer fmt.Println("Hola")
-	fmt.Println("Mundo")
+	// // Defer
+	// defer fmt.Println("Hola")
+	// fmt.Println("Mundo")
 
-	// Continue y  break
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
-		if i == 2 {
-			fmt.Println("Es 2")
-			continue
-		}
+	// // Continue y  break
+	// for i := 0; i < 10; i++ {
+	// 	fmt.Println(i)
+	// 	if i == 2 {
+	// 		fmt.Println("Es 2")
+	// 		continue
+	// 	}
 
-		if i == 8 {
-			fmt.Println("Es 8")
-			break
-		}
-	}
+	// 	if i == 8 {
+	// 		fmt.Println("Es 8")
+	// 		break
+	// 	}
+	// }
+
+	// Array
+	var array [4]int
+	array[0] = 1
+	array[1] = 2
+	fmt.Println(array, len(array), cap(array))
+
+	// slice
+	slice := []int{0, 1, 2, 3, 4, 5, 6}
+	fmt.Println(slice, len(slice), cap(slice))
+
+	// Metodos en el slice
+	fmt.Println(slice[0])
+	fmt.Println(slice[:3])
+	fmt.Println(slice[2:4])
+	fmt.Println(slice[4:])
+
+	// append
+	slice = append(slice, 7)
+	fmt.Println(slice)
+
+	// append nueva list
+	newSlice := []int{8, 9, 10}
+	slice = append(slice, newSlice...)
+	fmt.Println(slice)
 }
